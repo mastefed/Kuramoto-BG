1dd8d7b7d468388cd30f59e866f1d13947445fee
Federico Vincenzo Mastellone <federicomastellone@outlook.com>
2021-01-27 17:41:02 +0100

    OOP translation

--- phaseoscillator.py
+++ phaseoscillator.py
@@ -6,37 +6,54 @@ from scipy.integrate import odeint
 
 random.seed(42)
 
-N = 3
-initialvalues = numpy.zeros(N)
-time_start = 0
-time_end = 10
-omega = numpy.zeros(N)
+class phaseoscillators:
+    def __init__(self, numberofoscillators):
+        self.N = numberofoscillators
+        self.initialvalues = numpy.zeros(self.N)
+        self.omega = numpy.zeros(self.N)
 
-for i in numpy.arange(N):
-    initialvalues[i] = 2*numpy.pi*random.random()
-    omega[i] = 5.*random.random()
+    def printinfos(self):
+        print(f'Number of Phase Oscillators: {self.N}\n')
+        print(f'Start Time set at {self.time_start}\nEnd Time set at {self.time_end}')
+        print(f'Initial conditions are: {self.initialvalues}\nNatural frequencies are: {self.omega}\nk: {self.k}')
 
-def phaseoscillators_fun(x,t):
-    theta0 = x[0]
-    theta1 = x[1]
-    theta2 = x[2]
+    def settimes(self, time_start, time_end, time_points):
+        self.time_start = time_start
+        self.time_end = time_end
+        self.times = numpy.linspace(self.time_start, self.time_end, time_points)
 
-    dtheta0dt = omega[0] + k/N*numpy.sin(theta1 - theta0) + k/N*numpy.sin(theta2 - theta0)
-    dtheta1dt = omega[1] + k/N*numpy.sin(theta0 - theta1) + k/N*numpy.sin(theta2 - theta1)
-    dtheta2dt = omega[2] + k/N*numpy.sin(theta1 - theta2) + k/N*numpy.sin(theta1 - theta2)
+    def setinitialconditions(self):
+        for i in numpy.arange(self.N):
+            self.initialvalues[i] = 2*numpy.pi*random.random()
+            self.omega[i] = 5.*random.random()
+            self.k = random.random()
 
-    return [dtheta0dt, dtheta1dt, dtheta2dt]
+    def phaseoscillators_fun(self, x, t):
+        theta0 = x[0]
+        theta1 = x[1]
+        theta2 = x[2]
 
-time = numpy.linspace(time_start, time_end, 1000)
+        self.dtheta0dt = self.omega[0] + self.k/self.N*numpy.sin(theta1 - theta0) + self.k/self.N*numpy.sin(theta2 - theta0)
+        self.dtheta1dt = self.omega[1] + self.k/self.N*numpy.sin(theta0 - theta1) + self.k/self.N*numpy.sin(theta2 - theta1)
+        self.dtheta2dt = self.omega[2] + self.k/self.N*numpy.sin(theta1 - theta2) + self.k/self.N*numpy.sin(theta1 - theta2)
 
-kval = [0., 0.4, 0.9]
+        return [self.dtheta0dt, self.dtheta1dt, self.dtheta2dt]
 
-for i, k in enumerate(kval):
-    phaseoscillators_evo = odeint(phaseoscillators_fun, initialvalues, time)
-    plt.figure(i)
-    plt.polar(time, phaseoscillators_evo[:,0], label=f'theta0, k={k}')
-    plt.polar(time, phaseoscillators_evo[:,1], label=f'theta1, k={k}')
-    plt.polar(time, phaseoscillators_evo[:,2], label=f'theta2, k={k}')
-    plt.legend()
+    def evolve(self, function):
+        self.phaseoscillators_evo = odeint(function, self.initialvalues, self.times)
 
-plt.show()
+    def printpolar(self):
+        plt.polar(self.times, self.phaseoscillators_evo[:,0], label=f'theta0, k={self.k}')
+        plt.polar(self.times, self.phaseoscillators_evo[:,1], label=f'theta1, k={self.k}')
+        plt.polar(self.times, self.phaseoscillators_evo[:,2], label=f'theta2, k={self.k}')
+        plt.legend()
+        plt.show()
+
+
+threeoscillators = phaseoscillators(3)
+threeoscillators.settimes(0., 10., 1000)
+threeoscillators.setinitialconditions()
+threeoscillators.printinfos()
+
+threeoscillators.evolve(threeoscillators.phaseoscillators_fun)
+threeoscillators.printpolar()
