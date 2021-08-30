dec21c227944a81150547270010df1458321acde
Federico Vincenzo Mastellone <federicomastellone@outlook.com>
2021-08-30 11:16:32 +0200

    Update sampledsimulation.py

--- sampledsimulation.py
+++ sampledsimulation.py
@@ -18,8 +18,6 @@ if __name__ == "__main__":
     #list = [K12, K13, K21, K23, K31, K32]
     values_to_choose = [[val[0], fixedval, val[1], fixedval, fixedval, fixedval] for val in iterator]
 
-    #In questo modo values_to_choose[i] è una lista di K da testare
-
     num_subpop1 = 40
     num_subpop2 = 120
     num_subpop3 = 20
